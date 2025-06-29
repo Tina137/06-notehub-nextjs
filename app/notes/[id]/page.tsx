08c02f1f0e9568bdfc50d1827e5671b06ed468f0
Tina Landar <landartina@gmail.com>
2025-06-29 17:18:33 +0300

    second-commit_two

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   dehydrate,
 } from "@tanstack/react-query";
 import { getSingleNote } from "@/lib/api";
-import NoteDetailsClient from "./NoteDetails.client";
+import NoteDetailsClient from "./Note.client";
 
 type Props = {
   params: Promise<{ id: string }>;
