23247931028d7cc221b5ab66353552af6bf3f74f
Tina Landar <landartina@gmail.com>
2025-06-29 17:10:27 +0300

    second-commit

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,15 +1,19 @@
 "use client";
-import NoteList from "@/components/NoteList/NoteList";
-import SearchBox from "@/components/SearchBox/SearchBox";
-import Pagination from "@/components/Pagination/Pagination";
-import { fetchNotes } from "@/lib/api";
 import { useState } from "react";
-import { useDebounce } from "use-debounce";
 import css from "@/app/notes/page.module.css";
 import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import { fetchNotes } from "@/lib/api";
+import { useDebounce } from "use-debounce";
 
-const Notes = () => {
+// Components
+import NoteList from "@/components/NoteList/NoteList";
+import Pagination from "@/components/Pagination/Pagination";
+import NoteModal from "@/components/NoteModal/NoteModal";
+import SearchBox from "@/components/SearchBox/SearchBox";
+
+function App() {
   const [page, setPage] = useState(1);
+  const [isModalOpen, setisModalOpen] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [debouncedInput] = useDebounce(inputValue, 500);
 
@@ -21,11 +25,16 @@ const Notes = () => {
   const onPageChange = (selected: number) => {
     setPage(selected + 1);
   };
+  const modalOpen = () => {
+    setisModalOpen(true);
+  };
+  const modalClose = () => {
+    setisModalOpen(false);
+  };
   const changeInput = (e: string) => {
     setPage(1);
     setInputValue(e);
   };
-
   return (
     <>
       <div className={css.app}>
@@ -38,11 +47,15 @@ const Notes = () => {
               setPage={onPageChange}
             />
           )}
+          <button onClick={modalOpen} className={css.button}>
+            Create note +
+          </button>
         </header>
-        {data && <NoteList notes={data.notes} />}
+        {data && <NoteList notes={data?.notes} />}
+        {isModalOpen && <NoteModal onClose={modalClose} />}
       </div>
     </>
   );
-};
+}
 
-export default Notes;
+export default App;
